d5cdde156e2b1d22df8890ebbcc6d826bb2fea8c
schukark <schukark2004@gmail.com>
2026-01-02 13:47:08 +0300

    docs: added crate level docs and warnings for undocumented code

--- apps/compute/voting-core/src/lib.rs
+++ apps/compute/voting-core/src/lib.rs
@@ -1,3 +1,12 @@
+//! voting-core is the core computation library for the Rust Voting Compute Service
+//!
+//! It computes the result of the vote according to the chosen VotingRule strategy.
+//! VotingRule is modular, meaning you can choose a Scorer, a Decider and a TieBreaker as long as
+//! they have matching in/out types (the compiler will let you know).
+
+#![warn(missing_docs)]
+#![warn(clippy::missing_docs_in_private_items)]
+
 pub mod decider;
 pub mod profile;
 pub mod scorer;
