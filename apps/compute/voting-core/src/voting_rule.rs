0298cd4af87252cc679f674e93abb9d2390b518f
schukark <schukark2004@gmail.com>
2026-01-02 13:11:27 +0300

    refactor: made run fallible with generalizeable error type

--- apps/compute/voting-core/src/voting_rule.rs
+++ apps/compute/voting-core/src/voting_rule.rs
@@ -1,20 +1,62 @@
-use crate::{decider::Decider, scorer::Scorer, tie_breaker::TieBreaker, types::Profile};
+use std::fmt::Debug;
 
-pub struct VotingRule<S, D, T> {
+use crate::{decider::Decider, profile::Profile, scorer::Scorer, tie_breaker::TieBreaker};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum VotingRuleError<SE, DE, TE>
+where
+    SE: Debug,
+    DE: Debug,
+    TE: Debug,
+{
+    #[error(transparent)]
+    ScoringError(SE),
+    #[error(transparent)]
+    DecisionError(DE),
+    #[error(transparent)]
+    TieBreakError(TE),
+}
+
+pub struct VotingRule<S: Scorer, D: Decider, T: TieBreaker> {
     scorer: S,
     decider: D,
     tiebreaker: T,
 }
 
+type VotingRuleResult<S, D, T> = Result<
+    usize,
+    VotingRuleError<<S as Scorer>::Error, <D as Decider>::Error, <T as TieBreaker>::Error>,
+>;
+
 impl<S, D, T> VotingRule<S, D, T>
 where
-    S: Scorer,
+    S: Scorer<Output = D::Input>,
     D: Decider,
     T: TieBreaker,
+    <S as Scorer>::Error: Debug,
+    <D as Decider>::Error: Debug,
+    <T as TieBreaker>::Error: Debug,
 {
-    fn run(&self, profile: &Profile) -> usize {
-        let scores = S::compute_score(profile);
-        let candidates = D::decide(&scores);
-        T::tie_break(&candidates, profile)
+    pub fn new(scorer: S, decider: D, tiebreaker: T) -> Self {
+        Self {
+            scorer,
+            decider,
+            tiebreaker,
+        }
+    }
+
+    pub fn run(&self, profile: &Profile) -> VotingRuleResult<S, D, T> {
+        let scores = self
+            .scorer
+            .compute_score(profile)
+            .map_err(VotingRuleError::ScoringError)?;
+        let candidates = self
+            .decider
+            .decide(&scores)
+            .map_err(VotingRuleError::DecisionError)?;
+        self.tiebreaker
+            .tie_break(&candidates, profile)
+            .map_err(VotingRuleError::TieBreakError)
     }
 }
