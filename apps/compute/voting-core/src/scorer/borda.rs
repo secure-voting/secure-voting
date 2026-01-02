9dc65c3a743b22ec44faa351727ca06179f62878
schukark <schukark2004@gmail.com>
2026-01-03 01:27:42 +0300

    feat: added borda scoring

---
+++ apps/compute/voting-core/src/scorer/borda.rs
@@ -0,0 +1,73 @@
+//! Borda scorer implementation.
+//!
+//! Scores each candidate depending on its rank. The last one gets 0 points, the next one gets one more and so on.
+
+use rayon::prelude::*;
+
+use crate::{profile::Profile, scorer::Scorer};
+
+/// Borda scorer.
+///
+/// Gives n-1 points to the first candiate, n-2 to the second, ..., 1 to (n-1)th and 0 to the last.
+pub struct BordaScorer;
+
+/// Borda scorer error.
+///
+/// Returned only when the ballot scored is empty.
+#[derive(Error, Debug)]
+#[error("Empty ballots")]
+pub struct BordaScorerError;
+
+impl Scorer for BordaScorer {
+    type Output = Vec<usize>;
+
+    type Error = BordaScorerError;
+
+    fn compute_score(&self, profile: &Profile) -> Result<Self::Output, Self::Error> {
+        let n_voters = profile.n_voters();
+        let n_candidates = profile.n_candidates();
+
+        if n_candidates == 0 {
+            return Err(BordaScorerError);
+        }
+
+        Ok((0..n_voters)
+            .into_par_iter()
+            .map(|i| {
+                let mut tmp = vec![0; n_candidates];
+
+                for j in 0..n_candidates {
+                    tmp[profile[i][j]] = n - j - 1;
+                }
+
+                tmp
+            })
+            .reduce(
+                || vec![0; n_candidates],
+                |a, b| a.iter().zip(b.iter()).map(|(x, y)| x + y).collect(),
+            ))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_incorrect_no_candidates() {
+        let votes = vec![vec![], vec![]];
+
+        assert!(matches!(
+            BordaScorer.compute_score(&votes.try_into().unwrap()),
+            Err(BordaScorerError)
+        ))
+    }
+
+    #[test_case(vec![vec![3, 2, 1, 0], vec![3, 2, 1, 0], vec![0, 3, 2, 1]], vec![6, 7, 4, 1]; "simple example")]
+    fn test_correct_borda_ranking(votes: Vec<Vec<usize>>, answer: Vec<usize>) {
+        assert_eq!(
+            answer,
+            BordaScorer.compute_score(&votes.try_into().unwrap())
+        );
+    }
+}
