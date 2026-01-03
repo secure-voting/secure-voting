a4f92129718aa145d1f2103cdc770fb1d024ec82
schukark <schukark2004@gmail.com>
2026-01-04 01:36:00 +0300

    feat: added essential types, traits and rules to the prelude, organized it neatly

--- apps/compute/voting-core/src/prelude.rs
+++ apps/compute/voting-core/src/prelude.rs
@@ -1,25 +1,45 @@
-//! Standard Voting Rules
+//! Prelude.
 //!
-//! 1. Plurality
-//! 2. Approval voting q=2, 3
-//! 3. Inverse plurality
-//! 4. Borda
-//! 5. Black
-//! 6. Copeland I, II, III
-//! 7. Simpson (Maxmin)
-//! 8. Minmax
-//! 9. Strong q-Paretian simple majority
-//! 10. Strong q-Paretian plurality
-//! 11. Strongest q-Paretian simple majority
-//! 12. Condorcet practical
-//! 13. Threshold
-//! 14. Hare
-//! 15. Inverse Borda
-//! 16. Nanson
-//! 17. Coombs
+//! Re-exports all the necessary public API:
+//!
+//! 1. 17 pre-packaged voting rules.
+//! 2. Voting traits and adaptors.
+//! 3. Matrix types, RuleOutcome, CandidateID and the Profile type.
 
-pub use crate::voting_rules::anti_plurality::AntiPluralityRule;
-pub use crate::voting_rules::approval::ApprovalRule;
-pub use crate::voting_rules::black::BlackRule;
-pub use crate::voting_rules::borda::BordaRule;
-pub use crate::voting_rules::plurality::PluralityRule;
+// Voting rules:
+// 1. Plurality
+// 2. Approval voting q=2, 3
+// 3. Inverse plurality
+// 4. Borda
+// 5. Black
+// 6. Copeland I, II, III
+// 7. Simpson (Maxmin)
+// 8. Minmax
+// 9. Strong q-Paretian simple majority
+// 10. Strong q-Paretian plurality
+// 11. Strongest q-Paretian simple majority
+// 12. Condorcet practical
+// 13. Threshold
+// 14. Hare
+// 15. Inverse Borda
+// 16. Nanson
+// 17. Coombs
+pub use crate::voting_rules::{
+    anti_plurality::AntiPluralityRule, approval::ApprovalRule, black::BlackRule, borda::BordaRule,
+    plurality::PluralityRule,
+};
+
+// Voting traits.
+pub use crate::{decider::Decider, scorer::Scorer, tie_breaker::TieBreaker};
+
+// Voting adaptors.
+//
+// Allow to combine the voting rules together or modify existing voting rules.
+pub use crate::voting_rules::adaptors::*;
+
+// Essential types.
+pub use crate::{
+    matrix::{CondorcetMatrix, PairwiseMatrix},
+    profile::{CandidateId, Profile},
+    tie_breaker::RuleOutcome,
+};
