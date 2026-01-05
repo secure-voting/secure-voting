cb48e9db962cb858a53d961fb8c8b129138183e9
schukark <schukark2004@gmail.com>
2026-01-05 13:04:37 +0300

    feat: added acceptIf adaptor that accepts or rejects the whole candidate set on a predicate

---
+++ apps/compute/voting-core/src/voting_rules/adaptors/accept_if.rs
@@ -0,0 +1,30 @@
+//! AcceptIf adaptor module.
+
+use crate::prelude::{Profile, RuleOutcome, VotingRuleExec};
+
+/// AcceptIf adaptor.
+///
+/// Accepts the candidate set if it meets a predicate F.
+pub struct AcceptIf<V, F> {
+    /// Voting rule to get the candidate set from.
+    voting_rule: V,
+    /// Predicate the would accept or reject the candidate set as a whole.
+    predicate: F,
+}
+
+impl<V, F> VotingRuleExec for AcceptIf<V, F>
+where
+    V: VotingRuleExec,
+    F: Fn(&RuleOutcome) -> bool,
+{
+    type Error = V::Error;
+
+    fn execute(&self, profile: &Profile) -> Result<RuleOutcome, Self::Error> {
+        let outcome = self.voting_rule.execute(profile)?;
+
+        match (self.predicate)(&outcome) {
+            true => Ok(outcome),
+            false => Ok(RuleOutcome::MultipleWinners(outcome.candidates())),
+        }
+    }
+}
--- apps/compute/voting-core/src/voting_rules/adaptors/mod.rs
+++ apps/compute/voting-core/src/voting_rules/adaptors/mod.rs
@@ -1,4 +1,5 @@
 //! Different adaptors that modify or compose voting strategies.
 
+pub mod accept_if;
 pub mod fallback;
 pub mod require_unique;
