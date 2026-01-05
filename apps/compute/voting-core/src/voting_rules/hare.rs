b1dce4d4ed578f52efae11015328d279cec13830
schukark <schukark2004@gmail.com>
2026-01-06 01:52:06 +0300

    fix: renamed HareRule from CoombsRule (copypaste remnants)

--- apps/compute/voting-core/src/voting_rules/hare.rs
+++ apps/compute/voting-core/src/voting_rules/hare.rs
@@ -14,5 +14,5 @@ use crate::{
 /// Do a regular plurality voting step.
 /// If there is a candidate with a strict majority of votes, they are the winner.
 /// Otherwise, eliminate the candidate with the *most* last-place votes and repeat until the winner is chosen.
-pub type CoombsRule<TB = FallthroughTieBreaker> =
+pub type HareRule<TB = FallthroughTieBreaker> =
     Elimination<PluralityScorer, MinScoreElimination, MajorityDecider, TB, MajorityStop>;
