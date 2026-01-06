0909d3f103c67324f2214bd9580f67ff1010395d
schukark <schukark2004@gmail.com>
2026-01-06 13:27:05 +0300

    test: added tests for the condorcet decider

--- apps/compute/voting-core/src/decider/condorcet.rs
+++ apps/compute/voting-core/src/decider/condorcet.rs
@@ -25,3 +25,33 @@ impl Decider for CondorcetDecider {
         Ok(vec![])
     }
 }
+
+// Unsafe code because CondorcetMatrix is constructed from a Vec<Vec<usize>>
+// in an "unsafe" matter (invariants are not upheld).
+#[allow(unsafe_code)]
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_condorcet_winner() {
+        let scores = unsafe {
+            CondorcetMatrix::new_unchecked(vec![vec![0, 1, 1], vec![0, 0, 0], vec![0, 1, 0]])
+        };
+
+        assert_eq!(
+            vec![CandidateId::new(0)],
+            CondorcetDecider.decide(&scores).unwrap()
+        );
+    }
+
+    #[test]
+    fn test_condorcet_cycle() {
+        let scores = unsafe {
+            CondorcetMatrix::new_unchecked(vec![vec![0, 1, 0], vec![0, 0, 1], vec![1, 0, 0]])
+        };
+        let answer: Vec<CandidateId> = vec![];
+
+        assert_eq!(answer, CondorcetDecider.decide(&scores).unwrap());
+    }
+}
