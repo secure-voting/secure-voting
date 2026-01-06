dc5529662dea3fa5d3000efc71218bd547fa6926
schukark <schukark2004@gmail.com>
2026-01-06 18:19:00 +0300

    test: added tests for the acceptIf adaptor

--- apps/compute/voting-core/src/voting_rules/adaptors/accept_if.rs
+++ apps/compute/voting-core/src/voting_rules/adaptors/accept_if.rs
@@ -14,6 +14,16 @@ pub struct AcceptIf<V, F> {
     predicate: F,
 }
 
+impl<V, F> AcceptIf<V, F> {
+    /// Construct a new AcceptIf instance.
+    pub fn new(voting_rule: V, predicate: F) -> Self {
+        Self {
+            voting_rule,
+            predicate,
+        }
+    }
+}
+
 impl<V, F> VotingRuleExec for AcceptIf<V, F>
 where
     V: VotingRuleExec,
@@ -29,5 +39,76 @@ where
         } else {
             Ok(RuleOutcome::MultipleWinners(outcome.candidates()))
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::profile::CandidateId;
+
+    use super::*;
+    use mockall::mock;
+
+    mock! {
+        pub SuccessfulVotingRule {
+
+        }
+
+        impl VotingRuleExec for SuccessfulVotingRule {
+            type Error = ();
+
+            fn execute(&self, profile: &Profile) -> Result<RuleOutcome, <Self as VotingRuleExec>::Error>;
+        }
+    }
+
+    fn fake_profile() -> Profile {
+        Profile::try_from(vec![vec![0, 2, 1]]).unwrap()
+    }
+
+    #[test]
+    fn test_does_match_outcome() {
+        let mut mock = MockSuccessfulVotingRule::new();
+
+        mock.expect_execute()
+            .return_const(Ok(RuleOutcome::UniqueWinner(CandidateId::new(1))));
+
+        assert_eq!(
+            Ok(RuleOutcome::UniqueWinner(CandidateId::new(1))),
+            AcceptIf::new(mock, |outcome: &RuleOutcome| outcome.is_unique())
+                .execute(&fake_profile())
+        );
+    }
+
+    #[test]
+    fn test_doesnt_match_outcome() {
+        let mut mock = MockSuccessfulVotingRule::new();
+
+        mock.expect_execute()
+            .return_const(Ok(RuleOutcome::MultipleWinners(vec![
+                CandidateId::new(1),
+                CandidateId::new(2),
+            ])));
+
+        assert_eq!(
+            Ok(RuleOutcome::MultipleWinners(vec![
+                CandidateId::new(1),
+                CandidateId::new(2)
+            ])),
+            AcceptIf::new(mock, |outcome: &RuleOutcome| outcome.is_unique())
+                .execute(&fake_profile())
+        );
+    }
+
+    #[test]
+    fn test_voting_rule_error_is_propagated() {
+        let mut mock = MockSuccessfulVotingRule::new();
+
+        mock.expect_execute().return_const(Err(()));
+
+        assert_eq!(
+            Err(()),
+            AcceptIf::new(mock, |outcome: &RuleOutcome| outcome.is_unique())
+                .execute(&fake_profile())
+        );
     }
 }
