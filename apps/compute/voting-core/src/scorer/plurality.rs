45574678c05bacafec42e91d64362e0d75d697e7
schukark <schukark2004@gmail.com>
2026-01-08 21:33:59 +0300

    refactor: changed plurality impl to use Score<T>

--- apps/compute/voting-core/src/scorer/plurality.rs
+++ apps/compute/voting-core/src/scorer/plurality.rs
@@ -6,7 +6,10 @@ use std::convert::Infallible;
 
 use rayon::prelude::*;
 
-use crate::{profile::Profile, scorer::Scorer};
+use crate::{
+    profile::Profile,
+    scorer::{Score, Scorer},
+};
 
 /// Plurality scorer.
 ///
@@ -17,22 +20,25 @@ impl Scorer for PluralityScorer {
     type Error = Infallible;
     type Output = Vec<usize>;
 
-    fn compute_score(&self, profile: &Profile) -> Result<Self::Output, Self::Error> {
+    fn compute_score(&self, profile: &Profile) -> Result<Score<Self::Output>, Self::Error> {
         let n_voters = profile.n_voters();
         let n_candidates = profile.n_candidates();
 
-        Ok((0..n_voters)
-            .into_par_iter()
-            .map(|i| {
-                let mut tmp = vec![0; n_candidates];
-                tmp[profile[i][0].into_inner()] = 1;
+        Ok(Score::new(
+            (0..n_voters)
+                .into_par_iter()
+                .map(|i| {
+                    let mut tmp = vec![0; n_candidates];
+                    tmp[profile.get_candidate_id(&profile[i][0]).unwrap()] = 1;
 
-                tmp
-            })
-            .reduce(
-                || vec![0; n_candidates],
-                |a, b| a.iter().zip(b.iter()).map(|(x, y)| x + y).collect(),
-            ))
+                    tmp
+                })
+                .reduce(
+                    || vec![0; n_candidates],
+                    |a, b| a.iter().zip(b.iter()).map(|(x, y)| x + y).collect(),
+                ),
+            profile.active_candidates(),
+        ))
     }
 }
 
@@ -47,7 +53,11 @@ mod tests {
 
         assert_eq!(
             answer,
-            scorer.compute_score(&votes.try_into().unwrap()).unwrap()
+            scorer
+                .compute_score(&votes.try_into().unwrap())
+                .unwrap()
+                .score()
+                .clone()
         );
     }
 }
