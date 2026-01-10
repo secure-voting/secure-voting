85caf40116b5f3b153d97617eb34672fb1793904
schukark <schukark2004@gmail.com>
2026-01-10 14:39:53 +0300

    feat: added more wiki tests, Q ranging from 1 to 6

--- apps/compute/voting-core/tests/approval_e2e.rs
+++ apps/compute/voting-core/tests/approval_e2e.rs
@@ -1,16 +1,54 @@
-use voting_core::prelude::*;
+use voting_core::{
+    prelude::*, scorer::approval::ApprovalScorerError, voting_rules::voting_rule::VotingRuleError,
+};
 
-use crate::common::{CHATTANOOGA, construct_tennessee_wiki_example};
+use crate::common::{CHATTANOOGA, MEMPHIS, NASHVILLE, construct_tennessee_wiki_example};
 
 mod common;
 
 #[test]
-fn test_wiki_example() {
+fn test_wiki_example_q_1() {
+    let profile = construct_tennessee_wiki_example();
+    let scorer = ApprovalRule::<1>::default();
+
+    assert_eq!(
+        RuleOutcome::UniqueWinner(CandidateId::new(MEMPHIS)),
+        scorer.execute(&profile).unwrap()
+    );
+}
+
+#[test]
+fn test_wiki_example_q_2() {
     let profile = construct_tennessee_wiki_example();
     let scorer = ApprovalRule::<2>::default();
 
     assert_eq!(
-        RuleOutcome::UniqueWinner(CandidateId::new(CHATTANOOGA)),
+        RuleOutcome::UniqueWinner(CandidateId::new(NASHVILLE)),
         scorer.execute(&profile).unwrap()
     );
+}
+
+#[test]
+fn test_wiki_example_q_3() {
+    let profile = construct_tennessee_wiki_example();
+    let scorer = ApprovalRule::<3>::default();
+
+    assert_eq!(
+        RuleOutcome::MultipleWinners(vec![
+            CandidateId::new(NASHVILLE),
+            CandidateId::new(CHATTANOOGA)
+        ]),
+        scorer.execute(&profile).unwrap()
+    );
+}
+
+#[test]
+fn test_error_q_too_big() {
+    let profile = construct_tennessee_wiki_example();
+    let scorer = ApprovalRule::<6>::default();
+
+    assert!(matches!(
+        scorer.execute(&profile),
+        Err(VotingRuleError::ScoringError(ApprovalScorerError))
+    ));
 }
