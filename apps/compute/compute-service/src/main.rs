2efa64c5efab2188d2f304d810a9549e62ddfa4a
schukark <schukark2004@gmail.com>
2026-01-24 18:59:10 +0300

    feat: added a build script and a skeleton implementation of the service

--- apps/compute/compute-service/src/main.rs
+++ apps/compute/compute-service/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use crate::securevoting::compute::v1::compute_server::{Compute, ComputeServer};
+
+pub mod securevoting {
+    pub mod compute {
+        pub mod v1 {
+            tonic::include_proto!("securevoting.compute.v1");
+        }
+    }
+}
+
+use securevoting::compute::v1::{RunChunk, RunResult};
+
+#[derive(Debug, Default)]
+struct ComputeService;
+
+#[tonic::async_trait]
+impl Compute for ComputeService {
+    async fn run(
+        &self,
+        request: tonic::Request<tonic::Streaming<RunChunk>>,
+    ) -> Result<tonic::Response<RunResult>, tonic::Status> {
+        todo!()
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let greeter = ComputeService;
+
+    tonic::transport::Server::builder()
+        .add_service(ComputeServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
