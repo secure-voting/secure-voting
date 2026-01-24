3db903bab4ce8533df51b304a79d8f64b73fc46b
schukark <schukark2004@gmail.com>
2026-01-24 20:51:19 +0300

    feat: added run_election to the prelude

--- apps/compute/voting-core/src/prelude.rs
+++ apps/compute/voting-core/src/prelude.rs
@@ -56,8 +56,9 @@ pub use crate::{
 // Allow to combine the voting rules together or modify existing voting rules.
 pub use crate::voting_rules::adaptors::*;
 
-// Essential types.
+// Essential types and functions.
 pub use crate::{
+    election::run_election,
     matrix::{CondorcetMatrix, PairwiseMatrix},
     profile::{CandidateId, Profile},
     tie_breaker::RuleOutcome,
