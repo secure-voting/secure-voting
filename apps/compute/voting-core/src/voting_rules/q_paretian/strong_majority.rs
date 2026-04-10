b37c5cd679fc77e3c47779e6d1db2d4df843d149
schukark <schukark2004@gmail.com>
2026-04-10 14:33:46 +0300

    fix: added Default and Debug derives to qParetian family of algos

--- apps/compute/voting-core/src/voting_rules/q_paretian/strong_majority.rs
+++ apps/compute/voting-core/src/voting_rules/q_paretian/strong_majority.rs
@@ -18,6 +18,7 @@ use crate::{
 ///
 /// Citation:
 /// 1. Aleskerov, F., Kurbanov, E. Degree of manipulability of social choice procedures. In: Alkan, A., Aliprantis, C.D., Yannelis, N.C. (eds) Current Trends in Economics. Studies in Economic Theory, v.8. 1999, Springer, Berlin, Heidelberg. doi: 10.1007/978-3-662-03750-8_2
+#[derive(Default, Debug)]
 pub struct SimpleMajorityRule<const LIMIT: usize>;
 
 impl<const LIMIT: usize> VotingRuleExec<RankingBallot> for SimpleMajorityRule<LIMIT> {
